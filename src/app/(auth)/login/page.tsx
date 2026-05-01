97cd259c96b9dad5b4b53d00163dfab6662921c6
BenedictoGeraldo <geraldodawa@gmail.com>
2026-05-02 02:06:46 +0700

    feat: slicing login and register page
    details:
     - you can access login page on /login
     - you can acces register page on /register
     - install remixicon for all icon usage
     - add poppins as default font family

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#F5EEE8] relative overflow-hidden">
+      <div className="absolute top-0 right-0 w-[300px] h-[300px] bg-[#B97A57]/20 rounded-full translate-x-1/3 -translate-y-1/3" />
+
+      <div className="absolute top-6 left-10 flex items-center gap-3">
+        <Image
+          src="/logo-kemhan.png"
+          alt="Logo Kemhan"
+          width={55}
+          height={55}
+          className="object-contain"
+        />
+        <div>
+          <h1 className="font-semibold text-[#000000] text-lg">
+            Pelayanan NCAGE
+          </h1>
+          <p className="text-sm text-[#374151]">Pusat Kodifikasi</p>
+        </div>
+      </div>
+
+      <div className="bg-[#FFFFFF] w-full max-w-md p-8 rounded-2xl shadow-lg z-10">
+        <h2 className="text-center text-[#8B1E1E] font-semibold text-xl mb-6">
+          Selamat Datang di Layanan NCAGE
+        </h2>
+
+        <div className="mb-4">
+          <label className="block text-sm text-[#374151] mb-1">Email</label>
+          <input
+            type="email"
+            placeholder="Masukkan alamat email"
+            className="w-full px-4 text-gray-500 py-2 border border-[#E5E7EB] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#8B1E1E]"
+          />
+        </div>
+
+        <div className="mb-2">
+          <label className="block text-sm text-[#374151] mb-1">
+            Kata Sandi
+          </label>
+          <input
+            type="password"
+            placeholder="Masukkan Kata Sandi"
+            className="w-full px-4 text-gray-500 py-2 border border-[#E5E7EB] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#8B1E1E]"
+          />
+        </div>
+
+        <div className="text-right mb-4">
+          <Link
+            href="/forgot-password"
+            className="text-sm text-[#8B1E1E] hover:underline"
+          >
+            Lupa Kata Sandi?
+          </Link>
+        </div>
+
+        <button className="w-full bg-[#5D3A3A] text-white py-3 rounded-lg font-semibold hover:opacity-90 transition">
+          Masuk
+        </button>
+
+        <p className="text-center text-sm mt-4 text-[#000000]">
+          Belum memiliki akun?{" "}
+          <Link href="/register" className="text-[#8B1E1E] font-medium">
+            Daftar
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
